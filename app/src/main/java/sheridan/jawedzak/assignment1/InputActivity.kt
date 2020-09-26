ea8a7f191f6abb0351ea00348d3b73b07d544b5d
Asad Jawed Zaki <jawedzak@sheridancollege.ca>
2020-09-25 22:31:02 -0400

    - Added InputActivity and OutputActivity file - Added some import statements - Added Layout file for both activities - Set the value of Viewbinding to "true" in gradle file

---
+++ app/src/main/java/sheridan/jawedzak/assignment1/InputActivity.kt
@@ -0,0 +1,13 @@
+package sheridan.jawedzak.assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sheridan.jawedzak.assignment1.databinding.ActivityInputBinding
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
---
+++ app/src/main/java/sheridan/jawedzak/assignment1/OutputActivity.kt
@@ -0,0 +1,8 @@
+package sheridan.jawedzak.assignment1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sheridan.jawedzak.assignment1.databinding.ActivityOutputBinding
+
+class OutputActivity : AppCompatActivity() {
+}
